6c7323c5cc298c14d3fd4ac63072f8c45f927c9a
kitteyandkat <57220112+kitteyandkat@users.noreply.github.com>
2024-05-25 17:40:48 -0400

    nested folder deletion

---
+++ src/sections/Products.jsx
@@ -0,0 +1,249 @@
+import { motion } from 'framer-motion';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import React, {  useLayoutEffect, useRef } from 'react';
+import styled from 'styled-components';
+
+import bottle from "../assets/Images/bottle.webp";
+// import camomileflower from "../assets/Images/camomile-flowers.webp";
+// import cardamonpods from "../assets/Images/cardamon-pods.webp";
+// import cardamon from "../assets/Images/cardamon-pods.webp";
+// import elderberries from "../assets/Images/elderberries.webp";
+// import juniperberries from "../assets/Images/juniper-berries.webp";
+// import kitchenglass from "../assets/Images/kitchen-glass.webp";
+// import orangepeels from "../assets/Images/orange-peels.webp";
+// import rosehips from "../assets/Images/rosehips.webp";
+// import staranise from "../assets/Images/star-anise.webp";
+
+const Section = styled.section`
+  min-height: 100vh;
+  /* height: auto; */
+  width: 100%;
+  margin: 0 auto;
+  /* height: 300vh; */
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+  /* background-color: ${(props) => props.theme.text}; */
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30vw;
+  height: 90vh;
+  box-shadow: 0 0 0 5vw ${(props) => props.theme.text};
+  border: 3px solid black;
+
+  z-index: 11;
+
+  @media (max-width: 70em) {
+  width: 40vw;
+
+    height: 80vh;
+  }
+
+  @media (max-width: 64em) {
+  width: 50vw;
+  box-shadow: 0 0 0 60vw ${(props) => props.theme.text};
+
+    height: 80vh;
+  }
+  @media (max-width: 48em) {
+  width: 60vw;
+
+    height: 80vh;
+  }
+  @media (max-width: 30em) {
+  width: 80vw;
+
+    height: 60vh;
+  }
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: 0%;
+  left: 50%;
+  transform: translate(-50%, 0%);
+  width: 25vw;
+  height: auto;
+  /* background-color: yellow; */
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 64em) {
+  width: 30vw;
+
+
+  }
+  @media (max-width: 48em) {
+  width: 40vw;
+
+  }
+  @media (max-width: 30em) {
+  width: 60vw;
+
+  }
+`;
+
+const Title = styled(motion.h1)`
+  font-size: ${(props) => props.theme.fontxxxl};
+  font-family: 'Metamorphous';
+  font-weight: 300;
+  /* text-transform: capitalize; */
+  color: ${(props) => props.theme.body};
+  text-shadow: 1px 1px 1px ${(props) => props.theme.text};
+
+  position: absolute;
+  top: 2rem;
+  left: 1rem;
+  z-index: 15;
+
+  @media (max-width: 64em) {
+    font-size: ${(props) => props.theme.fontxxl};
+
+
+  }
+  @media (max-width: 48em) {
+    font-size: ${(props) => props.theme.fontxl};
+  
+  }
+`;
+const Text = styled.div`
+  width: 20%;
+  font-size: ${(props) => props.theme.fontlg};
+  font-weight: 300;
+  position: absolute;
+  padding: 2rem;
+  top: 0;
+  right: 0;
+  z-index: 11;
+
+  @media (max-width: 48em) {
+    display: none;
+  
+  }
+ 
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 5rem 0;
+
+  h2 {
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+    z-index: 5;
+  }
+`;
+const Photos = ({ img, name }) => {
+  return (
+    <Item>
+      <img width="400" height="600" src={img} alt={name} />
+      <h2>{name}</h2>
+    </Item>
+  );
+};
+
+const Products = () => {
+   gsap.registerPlugin(ScrollTrigger);
+  const ref = useRef(null);
+
+  const ScrollingRef = useRef(null);
+
+
+  useLayoutEffect(() => {
+    let element = ref.current;
+
+    let scrollingElement = ScrollingRef.current;
+let t1= gsap.timeline();
+    setTimeout(() => {
+      let mainHeight = scrollingElement.scrollHeight;
+      element.style.height = `calc(${mainHeight / 4}px)`;
+      t1.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: 'top top',
+          end: 'bottom+=100% top-=100%',
+          scroller: '.App', //locomotive-scroll
+          scrub: 1,
+          pin: true,
+          // markers: true,
+        },
+        ease: 'none',
+      });
+
+      t1.fromTo(
+        scrollingElement,
+        {
+          y: '0',
+        },
+        {
+          y: '-100%',
+          scrollTrigger: {
+            // id: `section-${index + 1}`,
+            trigger: scrollingElement,
+            start: 'top top',
+            end: 'bottom top',
+            scroller: '.App',
+            scrub: 1,
+            // markers: true,
+          },
+        },
+      );
+
+      ScrollTrigger.refresh();
+    }, 1000);
+    ScrollTrigger.refresh();
+
+    return () => {
+      t1.kill();
+      ScrollTrigger.kill();
+    };
+  }, []);
+
+  return (
+    <Section  ref={ref} id="fixed-target" className="products">
+      <Overlay />
+
+      <Title
+        data-scroll data-scroll-speed="-2" data-scroll-direction="horizontal"
+      >
+        Products
+      </Title>
+
+      <Container ref={ScrollingRef}>
+        <Photos img={bottle} name="Flagship Gin" />
+        <Photos img={bottle} name="Flagship Mini" />
+        <Photos img={bottle} name="Reserve Gin" />
+        <Photos img={bottle} name="Limited Edition Gin" />
+      </Container>
+
+      <Text data-scroll data-scroll-speed="-4">
+        Our products are available for purchase at our exquisite Tasting Room or online, where you can explore our collection of unique gin blends and exquisite bottles from the comfort of your home. Each blend is expertly crafted, blending traditional botanicals with innovative flavors to offer a truly exceptional tasting experience. Whether you're seeking a classic juniper-forward gin or a more adventurous infusion, our diverse range has something to suit every taste.
+        <br />
+        <br />
+        Our bottles are more than vessels for exceptional gin—they're works of art in their own right. Designed with attention to detail, each bottle tells a story, reflecting the spirit within. Whether adorned with intricate labels or minimalist elegance, our bottles stand as a testament to our commitment to craftsmanship and style.        <br />
+        <br />
+        Explore our product offerings and indulge in the beauty of our unique blends and bottles. From the first glance to the final sip, every aspect has been curated to delight the senses and elevate your gin journey. Welcome to a world where tradition meets innovation, and every bottle holds the promise of extraordinary flavor and experience.
+      </Text>
+    </Section>
+  );
+};
+
+export default Products;
