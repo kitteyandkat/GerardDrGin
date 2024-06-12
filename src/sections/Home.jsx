020d79e4659d1eedb4fcdbbecb26ce24b332a876
kitteyandkat <57220112+kitteyandkat@users.noreply.github.com>
2024-06-12 16:07:26 -0400

    hide cover video mobile

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,10 +1,7 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-// import CoverVideo from '../components/CoverVideo';
-// import Navbar from '../components/Navbar';
-// import Logo from './../components/Logo';
-
+// Lazy loading components
 const CoverVideo = React.lazy(() => import('../components/CoverVideo'));
 const Navbar = React.lazy(() => import('../components/Navbar'));
 const Logo = React.lazy(() => import('../components/Logo'));
@@ -12,17 +9,29 @@ const Logo = React.lazy(() => import('../components/Logo'));
 const Section = styled.section`
   position: relative;
   min-height: 100vh;
-overflow: hidden;
-
+  overflow: hidden;
 `;
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <Section  id="home">
+    <Section id="home">
       <Suspense fallback={<></>}>
         <Logo />
         <Navbar />
-        <CoverVideo />
+        {!isMobile && <CoverVideo />}
       </Suspense>
     </Section>
   );
